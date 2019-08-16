6687df56fb1dde6cdc9e96568d9b1214b4956413
BabaOlude <32584482+BabaOlude@users.noreply.github.com>
2019-08-15 21:27:20 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,51 +1,36 @@
 // Choose the color
 // Choose the size
+var height, width, color;
 
 // When the user pics the size call makeGrid
-
-function makeGrid() {
-	console.log("makeGrid is running!")
-	
-	// Select the size
-	
-	var canvas, cell, gridHeight, gridWidth, rows;
-	
-	canvas = $('#pixel_canvas');
-	gridHeight = $('#input_height').val();
-	gridWidth = $('#input_width').val();
-	
-	canvas.children().remove()
-	
-	for (x = 0; x < gridHeight; x++) {
-	canvas.append('<tr></tr>');
-	}
-	
-	rows = $('tr');
-	
-	for (y = 0; y < gridWidth; y++) {
-	rows.append('<td></td>');
-	} 
-	
-	cell = canvas.find('td');
-	
-	// Change the color when td is clicked by the user
-	cell.click(function() {
-		// Select color input
-		console.log("changeColor is running!");
-		var color;
-		color = $("#colorPicker").val();
-		$(this).attr('bgcolor', color);
-	});
-	
-}
-
-// When the user chooses the size call makeGrid
-var submitQuery;
-
-submitQuery = $('input[type="submit"]')
-
-submitQuery.click(function(event) {
+$('#sizePicker').submit(function (event){
   event.preventDefault();
-  makeGrid();
+  height = $('#inputHeight').val();
+  width = $('#inputWeight').val();
+  makeGrid(height, width);
+//height will be logged in the console
 });
 
+function makeGrid(x, y) {
+  $('tr').remove();
+	
+// Your code goes here!
+  for(var i=1; i <= x; i++){
+     $(#pixelCanvas').append('<tr id=table' + i + '></tr>');
+     for(var j = 1; j <= y; j++){
+	$('#table' + i).append('<td></td>');
+     }
+  }
+  // Choose color and add it to the gride
+  $('td').click(function addColor(){
+     color = $('#colorPicker').val();
+     if ($(this).attr('style')){
+	$(this).removeAttr('style');
+     }  else{
+	$(this).attr('style','background-color:' + color);
+     }
+	  
+  });
+	
+}
+	
