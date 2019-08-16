8366d2e06f9273b64c91472b2b48996ef10d89b2
BabaOlude <32584482+BabaOlude@users.noreply.github.com>
2019-08-16 09:59:52 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,35 +6,35 @@ var height, width, color;
 // When the user pics the size call makeGrid
 
 $('#sizePicker').submit(function (event){
-  event.preventDefault();
-  height = $('#inputHeight').val();
-  width = $('#inputWeight').val();
-  makeGrid(height, width);
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    makeGrid(height, width);
 	
 //height will be logged in the console
 	
 });
 
 function makeGrid(x, y) {
-  $('tr').remove();
+    $('tr').remove();
 	
 // Your code goes here!
 	
-  for(var i=1; i <= x; i++){
-     $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-     for(var j = 1; j <= y; j++){
-	$('#table' + i).append('<td></td>');
+    for(var i=1; i <= x; i++){
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+        for(var j = 1; j <= y; j++){
+	    $('#table' + i).append('<td></td>');
      }
   }
     	
-  // Choose color and add it to the grid
+// Choose color and add it to the grid
 	
-  $('td').click(function addColor(){
-     color = $('#colorPicker').val();
-     if ($(this).attr('style')){
-	$(this).removeAttr('style');
-     }  else{
-	$(this).attr('style','background-color:' + color);
+    $('td').click(function addColor(){
+        color = $('#colorPicker').val();
+        if ($(this).attr('style')){
+	    $(this).removeAttr('style');
+     }      else{
+	    $(this).attr('style','background-color:' + color);
      }
 	  
   });
